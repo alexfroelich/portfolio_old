c1b431e8f9589b11fb10d0c6a8f86395b8acb2a1
alexfroelich <38202702+alexfroelich@users.noreply.github.com>
2020-02-13 02:07:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+//Get the Cards section
+const cards = document.querySelector(".cards");
+
+    
+    
+
+//Create a new card
+const card = document.createElement('div');
+card.setAttribute('class', 'card');
+//Set card's attributes
+//title
+const sectionTitle = document.createElement('h2');
+sectionTitle.textContent = "sqShooter";
+const sectionImage = document.createElement('img');
+sectionImage.setAttribute('class', 'game-image');
+sectionImage.setAttribute('src', 'assets/castora.jpg');
+
+//Add the card
+cards.appendChild(card);
+//Add the attributes
+card.appendChild(sectionTitle);
+card.appendChild(sectionImage);
+
+*/
